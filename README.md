5bf10528c5ac050035e93bfa0bc59efaadce85b9
Alejandro Menocal <amenocal@github.com>
2023-07-25 09:40:44 -0500

    finished user web service

---
+++ README.md
@@ -0,0 +1,10 @@
+# Go WebService Example
+
+This is a simple example of a web service written in Go.
+
+Put together as part of learning and Go training.
+
+```bash
+go build .
+./go-web-service
+```
--- controllers/front.go
+++ controllers/front.go
@@ -1,10 +1,19 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
 
 func RegisterControllers() {
 	uc := newUserController()
 
 	http.Handle("/users", *uc)
 	http.Handle("/users/", *uc) // this is in order to handle both /users and /users/1 (for example)
+}
+
+func encodeResponseAsJSON(data interface{}, w io.Writer) {
+	enc := json.NewEncoder(w)
+	enc.Encode(data)
 }
